509c5347dc08cad2372e73ec5de8c35333e394f0
cryptokage2306 <26vivek06@gmail.com>
2024-02-07 17:01:50 +0530

    added res headers

--- dist/index.js
+++ dist/index.js
@@ -26,6 +26,12 @@ const corsOptions = {
     origin: 'https://nftworld-iota.vercel.app/',
 };
 app.use((0, cors_1.default)(corsOptions));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 const mongoURI = process.env.MONGO_URI;
 function connectToDatabase() {
     return __awaiter(this, void 0, void 0, function* () {

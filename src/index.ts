d9c988aa45b0e0c4381753b4465314674560c4b1
CryptoKage2306 <26vivek06@gmail.com>
2024-02-08 13:34:45 +0530

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,7 @@ const corsOptions = {
   origin: 'https://nftworld-iota.vercel.app/',
 };
 
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.use(cors());
 
 const mongoURI:any = process.env.MONGO_URI ;
 async function connectToDatabase() {
